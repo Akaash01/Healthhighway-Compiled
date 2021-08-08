3ad8815a6ae4b3a761c9ef709e75d198410845d3
Sapna2001 <harisapnanair@gmail.com>
2021-08-09 01:02:21 +0530

    login

---
+++ js/login.js
@@ -0,0 +1,16 @@
+var loginImageSm = document.getElementById("loginImageSm");
+var loginImageLg = document.getElementById("loginImageLg");
+var modalBg = document.querySelector(".login-modal-bg");
+var modalClose = document.querySelector(".close");
+
+loginImageLg.addEventListener("click", function() {
+    modalBg.classList.add("bg-active");
+});
+
+loginImageSm.addEventListener("click", function() {
+    modalBg.classList.add("bg-active");
+});
+
+modalClose.addEventListener("click",function() {
+    modalBg.classList.remove("bg-active");
+});
