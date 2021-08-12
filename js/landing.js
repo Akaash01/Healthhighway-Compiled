a5ba62cf47c1b9c82db5eb9e96f69f839fb4ba0a
Sapna2001 <harisapnanair@gmail.com>
2021-08-13 02:25:26 +0530

    navbar

--- js/landing.js
+++ js/landing.js
@@ -14,7 +14,7 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     var nav = document.querySelector("nav");
     if (scroll <= 32) {
-        nav.style.backgroundColor = "transparent";     
+        nav.style.backgroundColor = "transparent";    
     } else {
         nav.style.backgroundColor = "white";
     }
