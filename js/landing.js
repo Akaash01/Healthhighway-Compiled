1b87c0e709560df2275cf8b7e5ca18068b89a159
Sapna2001 <harisapnanair@gmail.com>
2021-08-13 00:59:02 +0530

    navbar

--- js/landing.js
+++ js/landing.js
@@ -9,3 +9,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var nav = document.querySelector("nav");
+    if (scroll <= 32) {
+        nav.style.backgroundColor = "transparent";     
+    } else {
+        nav.style.backgroundColor = "white";
+    }
+    console.log(nav)
+});
+
