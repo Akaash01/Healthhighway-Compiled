741843cb8e9a8a26efe1454e7fef919c5d1721e8
Vidhu Shekhar <twrvidhu18@gmail.com>
2021-10-07 15:00:48 +0530

    form update

---
+++ js/newform.js
@@ -0,0 +1,10 @@
+const timing_div = document.querySelector(".timing-slots")
+const routine_div = document.querySelector(".day-slots")
+timing_div.addEventListener('click', () => {
+    document.querySelector(".time-grid").style.display = "grid";
+    document.querySelector(".day-grid").style.display = "none";
+})
+routine_div.addEventListener('click', () => {
+    document.querySelector(".day-grid").style.display = "grid";
+    document.querySelector(".time-grid").style.display = "none";
+})
